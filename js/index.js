e687bb6de33c12e40d18f55d2113fec2a5b608eb
PrietoAlejandro <faospitia2@misena.edu.co>
2022-11-08 12:30:20 -0500

    evidencia

--- js/index.js
+++ js/index.js
@@ -6,32 +6,33 @@ sele.addEventListener('click', ()=>
     let apuesta =document.getElementById('apuestav').value
 
     let acumulado = 0;
-    let cantidadv;
     let opcion;
     let contador = 0; 
 
-    alert(`La moneda esta girado, espere un momento`)
     opcion=confirm("¿Desea empezar a jugar?")
+
+    alert(`La moneda esta girado, espere un momento`)
+    
     while(opcion == true)
-    {
+{
         random=Math.floor(Math.random() *2) +1;
     if(selec==1 && random==1)
     {
-        alert(random)
+        alert(`Esto es Cara`)
         alert(`Es correcto, usted gano`)
         acumulado=acumulado+(apuesta*2)
         alert(`Usted ha ganado, lleva acumulado: `+acumulado)
     }
     else if(selec==2 && random==2)
     {
-        alert(random)
+        alert(`Esto es Sello`)
         alert(`Es correcto, usted gano`)
         acumulado=acumulado+(apuesta*2)
         alert(`Usted ha ganado, lleva acumulado: `+acumulado)
     }
     else
     {
-        alert(random)
+        alert(`La moneda es diferente a su seleccion`)
         alert(`Usted ha perdido`)
         acumulado=acumulado-(apuesta)
         alert(`Usted ha perdido, acumulo: `+acumulado)
